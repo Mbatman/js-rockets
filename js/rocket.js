5e4b23f3018140f7b2456da93684f38c65c244e4
Mbatman <Mbatman@users.noreply.github.com>
2020-05-29 15:02:39 -0400

    Rocket complete

--- js/rocket.js
+++ js/rocket.js
@@ -2,17 +2,30 @@ var timer = null;
 var countdownNumber = 10;
 var changeState = function (state) {
 	document.body.className = 'body-state'+ state;
+	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById('countdown').innerHTML = countdownNumber;
+
 		//countdown
 	if (state == 2) {
 		timer = setInterval(function () {
-			document.getElementById('countdown').innerHTML = countdownNumber;
 			countdownNumber = countdownNumber-1;
+			document.getElementById('countdown').innerHTML = countdownNumber;
+			
+			if (countdownNumber > 4 && countdownNumber <=7) {
+				document.getElementById('nervous').className = 'nervous show'
+			} else{document.getElementById('nervous').className = 'nervous'}//be nervous
+
+			if (countdownNumber > 1 && countdownNumber <= 4) {
+				document.getElementById('cant-wait').className = 'cant-wait show'
+			} else{document.getElementById('cant-wait').className = 'cant-wait'}// cant wait
+			
 			if (countdownNumber <= 0) {changeState(3);
 			};
 		}, 500);
 	}
 	else if (state == 3){
-		var success = setTimeout(funtion ()
+		var success = setTimeout(function ()
 			{
 				var randomNumber = Math.round(Math.random()*10);
 			
@@ -22,5 +35,4 @@ var changeState = function (state) {
 					else {changeState(5);}
 
 			}, 2000);
-	};
-}
+	};}
